7a1d9b6c3ba4e22978fc6d19f5987afc468f4c9c
Omarchy <16846380+rafalfaro18@users.noreply.github.com>
2026-02-12 00:00:50 -0600

    adds fish config for cachyos. sets vim keybinds.

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+fish_vi_key_bindings
+
