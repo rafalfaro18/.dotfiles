917964eeb840ecde42b5965a17fb90279dc13a72
OldMac <oldmac@example.com>
2026-08-10 13:29:41 -0600

    Set TokyoNight color scheme for wezterm font 13

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,8 @@
 local wezterm = require 'wezterm'
 local config = {}
+
 config.font = wezterm.font 'JetBrainsMonoNL Nerd Font Mono'
+config.font_size = 13
+config.color_scheme = "Tokyo Night"
+
 return config
