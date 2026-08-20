ed98e283d64058ca9681ae50ae0a42399086fd8f
pi <pi-arch@example.com>
2026-08-19 23:40:11 -0600

    fix stretching of the wezterm background image

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,19 +4,17 @@ local config = {}
 config.font = wezterm.font 'JetBrainsMonoNL Nerd Font Mono'
 config.font_size = 13
 config.color_scheme = "tokyonight_night"
-config.window_background_image = wezterm.home_dir .. '/Pictures/wallhaven-vgyyxl.png'
-
-config.window_background_image_hsb = {
-  -- Darken the background image by reducing it to 1/3rd
-  brightness = 0.3,
-
-  -- You can adjust the hue by scaling its value.
-  -- a multiplier of 1.0 leaves the value unchanged.
-  hue = 1.0,
-
-  -- You can adjust the saturation also.
-  saturation = 1.0,
+config.background = {
+  {
+    source = { File = wezterm.home_dir .. '/Pictures/wallhaven-vgyyxl.png' },
+    width = 'Cover',
+    height = 'Cover',
+    horizontal_align = "Right",
+    vertical_align = "Top",
+    hsb = { brightness = 0.3 },
+  }
 }
+
 
 config.hide_tab_bar_if_only_one_tab = true
 --config.window_decorations = "NONE"
