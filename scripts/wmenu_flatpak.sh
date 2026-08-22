9ef20e2423f0bab7eb6d5aa0bc255ba8ff59fee8
pi <pi-arch@example.com>
2026-08-22 03:00:11 -0600

    script to launch in dwl wmenu flatpak apps

--- scripts/wmenu_flatpak.sh
+++ scripts/wmenu_flatpak.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Get a list of installed Flatpak app IDs
-choice=$(flatpak list --app --columns=application | wmenu -i -p "Run Flatpak:")
+choice=$(flatpak list --app --columns=application | wmenu -f "JetBrainsMonoNL Nerd Font Mono 16" -i -p "Run Flatpak:")
 
 # If you selected an app, run it
 if [ -n "$choice" ]; then
