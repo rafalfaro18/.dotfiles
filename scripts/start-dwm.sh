6afa3ade1fd4e93ea53d543f5d03ee2b4b9dca58
OldMac <oldmac@example.com>
2026-08-29 11:22:07 -0600

    perfect scale to 1920x1200

--- scripts/start-dwm.sh
+++ scripts/start-dwm.sh
@@ -2,7 +2,7 @@
 #hdmi
 xrandr --output HDMI-0 --mode 1920x1080
 #laptop
-xrandr --output DP-2 --mode 2880x1800 --rate 59.99 --scale 0.8x0.8
+xrandr --output DP-2 --mode 2880x1800 --rate 59.99 --scale 0.67x0.67
 # adjust resolution before wallpaper or it'll distort
 feh --bg-scale ~/Pictures/wallhaven-vgyyxl.png &
 # uncomment dunst if not autostarting service
