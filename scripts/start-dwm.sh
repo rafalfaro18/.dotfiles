f0c5610c521c94aec90ea5b983594a7d4eed9e09
OldMac <oldmac@example.com>
2026-08-29 11:08:23 -0600

    set resolution in dwm startup script instead of nvidia settings

--- scripts/start-dwm.sh
+++ scripts/start-dwm.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#hdmi
+xrandr --output HDMI-0 --mode 1920x1080
+#laptop
+xrandr --output DP-2 --mode 2880x1800 --rate 59.99 --scale 0.8x0.8
+# adjust resolution before wallpaper or it'll distort
 feh --bg-scale ~/Pictures/wallhaven-vgyyxl.png &
 # uncomment dunst if not autostarting service
 #dunst &
