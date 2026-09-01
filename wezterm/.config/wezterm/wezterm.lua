f39cde4703415455850bc104a768319ea3606a1a
Cachy <work@example.com>
2026-09-01 15:24:28 -0600

    wezterm: no padding

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -17,6 +17,13 @@ config.background = {
   }
 }
 
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 config.audible_bell = 'Disabled'
 
 config.hide_tab_bar_if_only_one_tab = true
