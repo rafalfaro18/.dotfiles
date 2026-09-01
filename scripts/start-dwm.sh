37ffc856c1d29c7be73d85512e77f9ffe1af1ea7
OldMac <oldmac@example.com>
2026-08-31 21:01:13 -0600

    set up my vertical ultrawide monitor

--- scripts/start-dwm.sh
+++ scripts/start-dwm.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
-#laptop
-#xrandr --output DP-2 --mode 2880x1800 --rate 59.99 --scale 0.67x0.67
-# disable laptop screen
-xrandr --output DP-2 --off
-#hdmi
-xrandr --output HDMI-0 --mode 2560x1440 --primary
-# Ultrawide Vertical
-xrandr --output DP-0 --mode 2560x1080 --rate 59.98 --rotate right
-# adjust resolution before wallpaper or it'll distort
-feh --bg-scale ~/Pictures/wallhaven-vgyyxl.png &
+
+# Give the X server a brief moment to fully initialize the hardware ports
+sleep 1
+
+# Re-initialize all connected screens to clear stuck Xorg cache
+xrandr --auto
+
 # uncomment dunst if not autostarting service
 #dunst &
 picom &
 slstatus &
+
+# Swap primary status FIRST, then alter layout and disable the laptop
+xrandr --output HDMI-0 --mode 2560x1440 --primary \
+       --output DP-0 --mode 2560x1080 --rate 59.98 --rotate right --right-of HDMI-0 \
+       --output DP-2 --off
+# Background applications
+feh --bg-scale ~/Pictures/wallhaven-vgyyxl.png &
+# Launch window manager
 exec dwm
